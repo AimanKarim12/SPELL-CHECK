0739b8bb083d3a3198707830b666202d41913cec
AimanKarim12 <113124249+AimanKarim12@users.noreply.github.com>
2022-12-16 09:54:48 -0700

    Update spellcheck.py

--- spellcheck-python-startcode-main/spellcheck.py
+++ spellcheck-python-startcode-main/spellcheck.py
@@ -20,8 +20,12 @@ def main():
             checkWordLinear(dictionary)
         elif selection =="2":
             checkWordBinary(dictionary)
+        elif selection =="3":
+            checkWordLinearAIW(aliceWords)
+        elif selection =="4":
+            checkWordBinaryAIW(aliceWords)
+            
 
-        
 # end main()
 
 def getMenuSelection():
@@ -31,11 +35,11 @@ def getMenuSelection():
     print("2. Spell Check a Word (Binary Search)")
     print("3. Spell Check Alice In Wonderland (Linear Search)")
     print("4. Spell Check Alice In Wonderland (Binary Search)")
-    print("5. Exit")
 
     #USER INPUT 
-    return input("Enter Selection (1-5): ")
+    return input("Enter Selection (1-4): ")
 
+#LINEAR 
 def checkWordLinear(dictionary):
     print("LINEAR SEARCH A WORD")
     userinput = input("Enter A Word: ")
@@ -50,12 +54,46 @@ def checkWordLinear(dictionary):
         print(userinput, "Is Found At: ", result)
     print("it took" , end - start, "Seconds")
 
+#BINARY
 def checkWordBinary(dictionary):
     print("BINARY SEARCH A WORD")
     userinput = input("Enter A Word: ")
     print("BINARY Search is Starting..")
     start = time.time()
     result = binarySearch(dictionary, userinput.lower())
+    end = time.time()
+
+    if result != -1:
+        print(userinput, "Is Found At", str(result))
+
+    else:
+        print(userinput, "Is Not Found")
+        
+    print("it took" , end - start, "Seconds")
+
+#LINEAR AIW
+def checkWordLinearAIW(dictionary, aliceWords):
+    count = 0
+    print("Linear Search is Starting..")
+    for word in aliceWords:
+        start = time.time()
+        result = linearSearch(dictionary, word)
+        end = time.time()
+        if(result == -1):
+            print(word, "Is Not Found")
+    else:
+        count += 1
+        print(f"total words from Alice Words Not In Dictionary: {count}")
+        print("it took" , end - start, "Seconds")
+
+
+#BINARY AIW
+def checkWordBinaryAIW(aliceWords):
+    print("BINARY SEARCH A WORD")
+    userinput = input("Enter A Word: ")
+    print("BINARY Search is Starting..")
+    start = time.time()
+    result = binarySearch(aliceWords, userinput.lower())
     end = time.time()
 
     if result != -1:
@@ -102,145 +140,3 @@ def binarySearch(array, item):
 
 # Call main() to begin program
 main()
-
-
-
-# PART A   
-# LOOP
-loop = True
-while loop: 
-
-#USER INPUT 
-    select = input("Enter Selection (1-5): ")
-    
-    #LINEAR SEARCH
-    if select == "1":
-        print("LINEAR SEARCH")
-        start = time.time()
-        userinput = input("Enter A Word: ")
-        def linearSearch(dictionary, userinput):
-            for i in range(len(dictionary)):
-                if (dictionary[i] == userinput):
-                    return i
-            return -1
-
-        dictionary = loadWordsFromFile("data-files/dictionary.txt")
-        result = linearSearch(dictionary, userinput)
-        if(result == -1):
-            print("Linear Search is Starting..")
-            print(userinput, "Is Not Found")
-            end = time.time()
-            print("it took" , end - start, "Seconds")
-        else:
-            print("Linear Search is Starting..")
-            print(userinput, "Is Found At: ", result)
-            end = time.time()
-            print("it took" , end - start, "Seconds")
-    
-    #BINARY SEARCH
-    elif select == "2":
-        print("BINARY SEARCH")
-        start = time.time()
-        userinput = input("Enter A Word: ")
-        def binarySearch(array, item):
-        
-            top = len(array) - 1
-            mid = 0
-            bottom = 0
-        
-            while bottom <= top:
-                mid = (top + bottom) // 2
-                if array[mid] < item:
-                    bottom = mid + 1
-                elif array[mid] > item:
-                    top = mid - 1
-                else:
-                    return mid
-            return -1
-            
-        dictionary = loadWordsFromFile("data-files/dictionary.txt")
-        item = userinput
-
-        result = binarySearch(dictionary, item)
-
-        if result != -1:
-            print("BINARY Search is Starting..")
-            print(userinput, "Is Found At", str(result))
-            end = time.time()
-            print("it took" , end - start, "Seconds")
-        else:
-            print("BINARY Search is Starting..")
-            print(userinput, "Is Not Found")
-            end = time.time()
-            print("it took" , end - start, "Seconds")
-
-    #PART B
-    #LINEAR SEARCH  AIW
-    elif select == "3":
-        print("LINEAR SEARCH (ALICE IN WONDERLAND)")
-        start = time.time()
-        userinput = input("Enter A Word: ")
-        def linearSearch(aliceWords, userinput):
-            for i in range(len(aliceWords)):
-                if (aliceWords[i] == userinput):
-                    return i
-            return -1
-
-        aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
-        result = linearSearch(aliceWords, userinput)
-        number_of_elements = len(aliceWords)
-        if(result == -1):
-            print("Linear Search is Starting..")
-            print(userinput, "Is Not Found")
-            end = time.time()
-            print("it took" , end - start, "Seconds")
-        else:
-            print("Linear Search is Starting..")
-            print("Number of the word:", userinput, "in the list: ", number_of_elements)
-            end = time.time()
-            print("it took" , end - start, "Seconds")
-    
-    #BINARY SEARCH AIW
-    elif select == "4":
-        print("BINARY SEARCH (ALICE IN WONDERLAND)")
-        start = time.time()
-        userinput = input("Enter A Word: ")
-        def binarySearch(array, item):
-        
-            top = len(array) - 1
-            mid = 0
-            bottom = 0
-        
-            while bottom <= top:
-                mid = (top + bottom) // 2
-                if array[mid] < item:
-                    bottom = mid + 1
-                elif array[mid] > item:
-                    top = mid - 1
-                else:
-                    return mid
-            return -1
-            
-        aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
-        item = userinput
-
-        result = binarySearch(aliceWords, item)
-        number_of_elements = len(userinput)
-
-        if result != -1:
-            print("BINARY Search is Starting..")
-            print("Number of the word:", userinput, "in the list: ", number_of_elements)
-            end = time.time()
-            print("it took" , end - start, "Seconds")
-        else:
-            print("BINARY Search is Starting..")
-            print(userinput, "Is Not Found")
-            end = time.time()
-            print("it took" , end - start, "Seconds")
-    
-    
-    #EXIT
-    elif select == "5":
-        print ("EXIT")
- 
-        loop = False
